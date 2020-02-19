0bfa451c340d17e64d62ebd31a6f24338618bbbe
Colton Steele <colton.steele@ou.edu>
2020-02-19 11:31:49 -0600

    Done for the day

---
+++ Participation-2-19/Participation-2-19/Quote.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Participation_2_19
+{
+    class Quote
+    {
+        public string quote { get; set; }
+        public string character{get; set;}
+        public override string ToString()
+        {
+            return quote + character;
+        }
+    }
+}
